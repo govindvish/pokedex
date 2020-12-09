5ca0be232199c685ef920558ab6c5c65590da0e4
Govind Vishwakarma <vishwakarmagovind22@gmail.com>
2020-12-09 22:48:37 +0530

    Setup initial app and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Pokedex } from './container/Pokedex';
+import { Pokemon } from './container/Pokemon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+        <Route
+          exact
+          path="/:pokemonId"
+          render={(props) => <Pokemon {...props} />}
+        />
+        <Redirect path="/" component={Pokemon} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/container/Pokedex.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Pokedex = () => {
+  return (
+    <div>
+      <h1>Pokedex</h1>
+    </div>
+  );
+};
